32cfb8034de779be092b5f7713ab904d51ae3202
Youhei Oonuma <oonuma617@gmail.com>
2020-09-04 08:34:16 +0900

    add

--- lib/tw.dart
+++ lib/tw.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold),
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(title: Text('card')),
         body: TwitterCard(),
@@ -15,16 +20,20 @@ class TwitterCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.yellow,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Icon(Icons.add),
           ),
-          Text(
-            'テキスト',
-            style: TextStyle(fontSize: 22.0),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('テキスト', style: Theme.of(context).textTheme.headline1),
+              Text('サブテキスト', style: Theme.of(context).textTheme.bodyText1),
+            ],
           )
         ],
       ),
