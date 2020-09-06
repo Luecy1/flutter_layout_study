cedd716c4769e8ac77ca9309757659283e8efef5
Youhei Oonuma <oonuma617@gmail.com>
2020-09-07 08:37:53 +0900

    flutter

--- lib/tw_access.dart
+++ lib/tw_access.dart
@@ -1,18 +1,19 @@
-import 'package:flutter_layout_study/secret_key.dart';
-import 'package:http/http.dart' as http;
-import 'package:twitter_api/twitter_api.dart';
-
-Future<void> main() async {
-
-  final twitter = twitterApi(
-    consumerKey: APIKey,
-    consumerSecret: APIKeySecret,
-    token: AccessToken,
-    tokenSecret: AccessTokenSecret,
-  );
-
-  final response =
-      (await twitter.getTwitterRequest('GET', 'statuses/home_timeline.json') as http.Response);
-
-  print(response.body);
-}
+//import 'package:flutter_layout_study/secret_key.dart';
+//import 'package:http/http.dart' as http;
+//import 'package:twitter_api/twitter_api.dart';
+//
+//Future<void> main() async {
+//  print('hello');
+//
+//  final twitter = twitterApi(
+//    consumerKey: APIKey,
+//    consumerSecret: APIKeySecret,
+//    token: AccessToken,
+//    tokenSecret: AccessTokenSecret,
+//  );
+//
+//  final response =
+//      (await twitter.getTwitterRequest('GET', 'statuses/home_timeline.json') as http.Response);
+//
+//  print(response.body);
+//}
