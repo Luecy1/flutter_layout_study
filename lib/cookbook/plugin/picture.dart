4499bf618d1426343838878071c7f09c6fe8448c
Youhei Oonuma <oonuma617@gmail.com>
2020-10-29 08:44:20 +0900

    complete camera

--- lib/cookbook/plugin/picture.dart
+++ lib/cookbook/plugin/picture.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,7 +12,9 @@ void main() async {
   final firstCamera = camera.first;
 
   runApp(MaterialApp(
-    home: _CameraPage(),
+    home: _CameraPage(
+      camera: firstCamera,
+    ),
   ));
 }
 
@@ -66,10 +72,38 @@ class __CameraPageState extends State<_CameraPage> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.camera_alt),
         onPressed: () async {
-          await _initializeControllerFuture;
-          final path
+          try {
+            await _initializeControllerFuture;
+            final path = join(
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()},png',
+            );
+            await _controller.takePicture(path);
+
+            Navigator.push(context, MaterialPageRoute(builder: (_) {
+              return _DisplayPicturePage(imagePath: path);
+            }));
+          } catch (e) {
+            print(e);
+          }
         },
       ),
+    );
+  }
+}
+
+class _DisplayPicturePage extends StatelessWidget {
+  final String imagePath;
+
+  _DisplayPicturePage({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Display the picture'),
+      ),
+      body: Image.file(File(imagePath)),
     );
   }
 }
