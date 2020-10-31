90d4d4a7cfa4442b96fe9d417939c570a62b6733
Youhei Oonuma <oonuma617@gmail.com>
2020-10-31 10:50:06 +0900

    add twitter

--- lib/tw/tw_access.dart
+++ lib/tw/tw_access.dart
@@ -1,20 +1,55 @@
-import 'package:http/http.dart' as http;
-import 'package:twitter_api/twitter_api.dart';
+import 'dart:convert';
+
+import 'package:twitter/twitter.dart';
 
 import 'secret_key.dart';
 
-Future<void> main() async {
-  print('hello');
+void main() async {
+  final twitterMediaList = await getTwitterMedia();
 
-  final twitter = twitterApi(
-    consumerKey: APIKey,
-    consumerSecret: APIKeySecret,
-    token: AccessToken,
-    tokenSecret: AccessTokenSecret,
+  for (var value in twitterMediaList) {
+    print(value);
+  }
+}
+
+Future<List<_twitterMedia>> getTwitterMedia() async {
+  final twitter = Twitter(
+    API_KEY,
+    API_KEY_SECRET,
+    ACCESS_TOKEN,
+    ACCESS_TOKEN_SECRET,
   );
-
   final response =
-      (await twitter.getTwitterRequest('GET', 'statuses/home_timeline.json') as http.Response);
+      await twitter.request('GET', 'favorites/list.json?include_entities=true&count=100');
+//  final response = await twitter.request('GET', 'statuses/home_timeline.json');
 
   print(response.body);
+
+  List<dynamic> jsonResponse = json.decode(response.body);
+  var twitterMediaList = <_twitterMedia>[];
+  for (var tw in jsonResponse) {
+    final extendedEntities = tw['extended_entities'];
+    if (extendedEntities != null) {
+      final media = extendedEntities['media'] as List<dynamic>;
+      if (media != null) {
+        for (var me in media) {
+          twitterMediaList.add(_twitterMedia(me['media_url_https']));
+        }
+      }
+    }
+  }
+  twitter.close();
+
+  return twitterMediaList;
+}
+
+class _twitterMedia {
+  final String mediaUrl;
+
+  _twitterMedia(this.mediaUrl);
+
+  @override
+  String toString() {
+    return '_twitterMedia{mediaUrl: $mediaUrl}';
+  }
 }
