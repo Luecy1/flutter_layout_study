91b6db59461bfa2be7637b1659352b8232957651
Youhei Oonuma <oonuma617@gmail.com>
2020-11-04 22:23:14 +0900

    drag

--- lib/cookbook/animation/PhysicsSimulation.dart
+++ lib/cookbook/animation/PhysicsSimulation.dart
@@ -11,7 +11,11 @@ class _PhysicsCardDragDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: _DraggableCard(
+        child: FlutterLogo(
+          size: 128,
+        ),
+      ),
     );
   }
 }
@@ -19,27 +23,57 @@ class _PhysicsCardDragDemo extends StatelessWidget {
 class _DraggableCard extends StatefulWidget {
   final Widget child;
 
-  _DraggableCard(this.child);
+  _DraggableCard({this.child});
 
   @override
   __DraggableCardState createState() => __DraggableCardState();
 }
 
-class __DraggableCardState extends State<_DraggableCard> {
+class __DraggableCardState extends State<_DraggableCard> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  Alignment _dragAlignment = Alignment.center;
+
+  DragUpdateDetails _details;
+
   @override
   void initState() {
+    _controller = AnimationController(vsync: this, duration: Duration(seconds: 1));
     super.initState();
   }
 
   @override
   void dispose() {
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: widget.child,
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Text('dx :${_details?.delta?.dx ?? ""}'),
+        GestureDetector(
+          onPanUpdate: (details) {
+            setState(() {
+              _dragAlignment += Alignment(
+                details.delta.dx / (size.width / 2),
+                details.delta.dy / (size.height / 2),
+              );
+              // print(_dragAlignment);
+              _details = details;
+              print('dy :${details.delta.dx}');
+            });
+          },
+          child: Align(
+            alignment: _dragAlignment,
+            child: Card(
+              child: widget.child,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
