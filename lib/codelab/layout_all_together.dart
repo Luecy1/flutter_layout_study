2192ad7be555b1b2247c86913492924ca45f009f
Youhei Oonuma <oonuma617@gmail.com>
2020-11-09 20:10:17 +0900

    layout_all_together.dart

---
+++ lib/codelab/layout_all_together.dart
@@ -0,0 +1,16 @@
+// https://flutter.dev/docs/codelabs/layout-basics#putting-it-all-together
+
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MaterialApp(
+    home: MainPage(),
+  ));
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
