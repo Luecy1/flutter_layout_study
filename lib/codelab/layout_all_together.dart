d5aa1058d639238057b5e7d26560f81bb4d48808
Youhei Oonuma <oonuma617@gmail.com>
2020-11-10 21:59:31 +0900

    Experience

--- lib/codelab/layout_all_together.dart
+++ lib/codelab/layout_all_together.dart
@@ -11,6 +11,34 @@ main() {
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: Container(
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.account_circle, size: 50.0),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Flutter McFlutter',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                Text(
+                  'Experienced App Developer',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
