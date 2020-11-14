e95fbdca1354a00ceb90af8ed1cd50e40cffadf3
Youhei Oonuma <oonuma617@gmail.com>
2020-11-14 22:56:38 +0900

    provider

--- lib/provider/model.dart
+++ lib/provider/model.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CartModel {
+class CartModel extends ChangeNotifier {
   CatalogModel _catalog;
 
   final List<int> _itemIds = [];
@@ -8,7 +8,29 @@ class CartModel {
   CatalogModel get catalog => _catalog;
 
   set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    assert(
+      _itemIds.every((id) => newCatalog.getById(id) != null),
+      'The catalog $newCatalog does not have one of $_itemIds in it.',
+    );
     _catalog = newCatalog;
+    notifyListeners();
+  }
+
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  int get totalPrice {
+    return items.fold(0, (total, current) => total + current.price);
+  }
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+    notifyListeners();
+  }
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+    notifyListeners();
   }
 }
 
