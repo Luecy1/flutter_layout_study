4661aaed2b96be33641bf6bc3ac0411f5a6882f0
Youhei Oonuma <oonuma617@gmail.com>
2020-11-14 12:12:24 +0900

    add provider

--- lib/provider/cart.dart
+++ lib/provider/cart.dart
@@ -1,9 +1,12 @@
 // https://github.com/flutter/samples/blob/master/provider_shopper/lib/screens/catalog.dart
 
 import 'package:flutter/material.dart';
+import 'package:flutter_layout_study/provider/model.dart';
+import 'package:flutter_layout_study/provider/theme.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: appTheme,
     home: MyCatalog(),
   ));
 }
@@ -29,17 +32,15 @@ class _MyAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      title: Text(
-        'Catalog',
-        style: Theme.of(context).textTheme.headline1,
-      ),
+      title: Text('Catalog', style: Theme.of(context).textTheme.headline1),
       floating: true,
       actions: [
         IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              // todo カート画面に遷移
-            }),
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {
+            // todo カート画面に遷移
+          },
+        ),
       ],
     );
   }
@@ -48,6 +49,39 @@ class _MyAppBar extends StatelessWidget {
 class _MyListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final item = CatalogModel().getById(0);
+
+    final textTheme = Theme
+        .of(context)
+        .textTheme
+        .headline6;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: LimitedBox(
+        maxHeight: 48.0,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 1.0,
+              child: Container(
+                color: item.color,
+              ),
+            ),
+            SizedBox(width: 24.0),
+            Expanded(child: Text('${item.name}', style: textTheme)),
+            SizedBox(width: 24.0),
+            _AddButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AddButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(onPressed: null, child: Text('ADD'));
   }
 }
