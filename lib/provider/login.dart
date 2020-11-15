283e5e418af3da273130a3e201ef531b38b11a59
Youhei Oonuma <oonuma617@gmail.com>
2020-11-16 08:39:48 +0900

    login

---
+++ lib/provider/login.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'theme.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyLogin(),
+    theme: appTheme,
+  ));
+}
+
+class MyLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(80.0),
+          child: Column(
+            children: [
+              Text(
+                'welcome',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'username'),
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'password'),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              RaisedButton(
+                color: Colors.yellow,
+                child: Text('Enter'),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
