8ca0b57c0644a5d5c8330a0bd05d08dd99747f24
Youhei Oonuma <oonuma617@gmail.com>
2020-12-01 08:37:12 +0900

    add rally

---
+++ lib/rally/main.dart
@@ -0,0 +1,27 @@
+// https://github.com/flutter/gallery/blob/master/lib/studies/rally/home.dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp());
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final tabBarView = Column();
+
+    return Container();
+  }
+}
